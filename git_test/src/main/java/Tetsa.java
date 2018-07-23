b3f7ee7aa994900fa58283b6e2b96e04b0d3870b
sublun <sublun@126.com>
2018-07-23 22:09:37 +0800

    第二次提交

--- git_test/src/main/java/Tetsa.java
+++
@@ -1,7 +0,0 @@
-public class Tetsa {
-    public static void main(String[] args) {
-        System.out.println("你好 啊，世界");
-        System.out.println("你好啊中国");
-        System.out.println("世界的美好");
-    }
-}
