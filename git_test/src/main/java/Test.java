d768b56f6640bfc1f48a6bc3ea268bc335c245c7
sublun <sublun@126.com>
2018-07-23 21:56:49 +0800

    第二次提交

--- git_test/src/main/java/Test.java
+++ git_test/src/main/java/Test.java
@@ -3,5 +3,6 @@ import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
 public class Test {
     public static void main(String[] args) {
         System.out.println("你好 啊，世界");
+        System.out.println("你好啊中国");
     }
 }
